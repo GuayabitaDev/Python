90018422d66506f99f550e917fa48bcc523fc3b8
Guayabita <92877638+Guayabitaa@users.noreply.github.com>
2022-06-01 07:40:54 -0500

    Create Programa2.py

---
+++ Programa2.py
@@ -0,0 +1,7 @@
+#Este programa convierte de Grados Farenheit a Celcius
+
+gradosFarenheit = int(input("Inserta los grados que convertir en Celcius en Farenehit: \n"))
+
+gradosCelcius = (gradosFarenheit - 32) * 5/9
+
+print(gradosCelcius)
