b6353ec0c3429ea20cdbb7e10192d1ad4002fbf3
Guayabita <92877638+Guayabitaa@users.noreply.github.com>
2022-06-01 01:44:30 -0500

    Create primerPrograma.py

---
+++ primerPrograma.py
@@ -0,0 +1,10 @@
+primerValor = int(input("Inserta el Primer Valor: "))
+segundoValor = int(input("Inserta el Segundo Valor: "))
+tercerValor = int(input("Inserta el Tercer Valor: "))
+
+print("El valor mas alto es: ")(max(primerValor, segundoValor, tercerValor))
+print("El valor mas bajo es: ")(min(primerValor, segundoValor, tercerValor))
+
+
+# min Esta funcion da el valor mas bajo de una lista otorgada
+# max Esta funcion da el valor mas alto de una lista otorgada
