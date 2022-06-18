6420e1c80d41b0c438a3a4689ed5766f7635f6ef
Guayabita <92877638+Guayabitaa@users.noreply.github.com>
2022-06-18 14:02:30 -0500

    Delete Programa3.py

--- Programa3.py
+++
@@ -1,7 +0,0 @@
-#Este programa convierte de $MXN a $USD
-
-MXN = float(input("Dime la cantidad de Pesos MXN que quieres convertir a USD: "))
-
-USD = MXN / 19.63 #Valor de Junio 1 del 2022
-
-print(USD)
